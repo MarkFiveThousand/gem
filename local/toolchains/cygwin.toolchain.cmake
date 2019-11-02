4091e5866d45cea858f260026b7d5bf751b90073
Mark Hodges <markhodges5000@gmail.com>
2019-11-02 10:21:13 -0700

    Added toolchain files, gettext, glib, libffi, and libiconv.

---
+++ local/toolchains/cygwin.toolchain.cmake
@@ -0,0 +1,28 @@
+# The name of the target operating system.
+SET(CMAKE_SYSTEM_NAME Cygwin)
+
+# Which compilers to use for C and C++.
+SET(CMAKE_C_COMPILER gcc)
+SET(CMAKE_CXX_COMPILER g++)
+
+
+# Where the target environment located.
+list(APPEND CMAKE_FIND_ROOT_PATH /usr/bin)
+list(REMOVE_DUPLICATES CMAKE_FIND_ROOT_PATH)
+
+# NOTE: I found this code snippet.  It seemed like it will come in handy.  I
+#       haven't played with it yet.
+#
+# Adjust the behavior of pkg-config.
+# https://stackoverflow.com/questions/9221236/pkg-config-fails-to-find-package-under-sysroot-directory
+#set(CMAKE_SYSROOT "/path/to/sysroot")
+#set(ENV{PKG_CONFIG_DIR} "")
+#set(ENV{PKG_CONFIG_LIBDIR} "${CMAKE_SYSROOT}/usr/lib/pkgconfig:${CMAKE_SYSROOT}/usr/share/pkgconfig")
+#set(ENV{PKG_CONFIG_SYSROOT_DIR} ${CMAKE_SYSROOT})
+
+# Adjust the default behavior of the FIND_XXX() commands: search headers and
+# libraries in the target environment, search programs in the host environment.
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
