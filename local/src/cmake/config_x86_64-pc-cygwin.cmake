2e31de25a2edc8f841a779f9d63ad864360e70af
Mark <markhodges5000@gmail.com>
2020-03-07 18:39:59 -0800

    Minor changes to config...cmake files.

---
+++ local/src/cmake/config_x86_64-pc-cygwin.cmake
@@ -0,0 +1,19 @@
+#  ___________                           __________
+# /\   _______\                         /\   ______\
+# \ \  \______/_     ___   ____ ____ ___\ \  \_____/_     ___   ___ ___  ______   ____    ___
+#  \ \  \ /\__  \  /'___`\/\   V    V   \\ \_______  \  /'   `\/\  \\  \/\   __\/' ._`\ /'___`\
+#   \ \  \\/__\  \/\  ___/\ \  \/\  \/\  \\/_______\  \/\  \\  \ \  \\  \ \  \ /\  \__//\  ___/
+#    \ \__________\ \____\ \ \__\ \__\ \__\ /\_________\ \_____/\ \______\ \__\\ \____/\ \____\
+#     \/__________/\/____/  \/__/\/__/\/__/ \/_________/\/____/  \/______/\/__/ \/___/  \/____/
+
+
+cmake_minimum_required (VERSION 3.11)
+
+gem_Msg ("------------ ${CMAKE_CURRENT_LIST_FILE}")
+
+# *FIX: This has no effect.  cmake is using /bin/gmake
+#set (CMAKE_MAKE_PROGRAM /bin/make)
+
+# Include settings common to all platforms.
+include ($ENV{gem_fullpath_gem}/local/src/cmake/config_common.cmake)
+
